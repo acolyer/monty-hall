6654c7a5ca7e5c0c764ece922c26ae32547a4ca6
Scott Andrews <scothis@gmail.com>
2012-10-09 17:19:11 -0400

    Apply Access-Control-Allow-Origin header to all reqeusts
    Previously the postHandle hook was use to add this header, however, it
    would not be added if the request was already complete. Controller
    handers that return a ResponseEntity provide their own headers and
    complete the response, avoiding the CorsInterceptor postHandle.
    
    Other minor polish.

--- src/main/java/org/springsource/samples/montyhall/web/CorsInterceptor.java
+++ src/main/java/org/springsource/samples/montyhall/web/CorsInterceptor.java
@@ -2,15 +2,12 @@ package org.springsource.samples.montyhall.web;
 
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
-import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class CorsInterceptor extends HandlerInterceptorAdapter {
 
-	private static final String ORIGIN = "Origin";
-	private static final String ALLOWED_METHODS = "GET, PUT, POST";
-	private static final String EXPOSED_HEADERS = "Location, ETag";
+	private static final String LOCATION = "Location";
 	private static final String ACCESS_CONTROL_ALLOW_ORIGIN = "Access-Control-Allow-Origin";
 	private static final String ACCESS_CONTROL_ALLOW_METHODS = "Access-Control-Allow-Methods";
 	private static final String ACCESS_CONTROL_ALLOW_HEADERS = "Access-Control-Allow-Headers";
@@ -19,34 +16,31 @@ public class CorsInterceptor extends HandlerInterceptorAdapter {
 	private static final String ACCESS_CONTROL_REQUEST_HEADERS = "Access-Control-Request-Headers";
 	private static final String ACCESS_CONTROL_MAX_AGE = "Access-Control-Max-Age";
 	private static final String CACHE_SECONDS = "300";
-	
-	@Override
-	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, 
-	                       ModelAndView modelAndView) {
-		// Our REST API is accessible from anywhere
-		response.setHeader(ACCESS_CONTROL_ALLOW_ORIGIN, "*");
-		response.setHeader(ACCESS_CONTROL_EXPOSE_HEADERS, EXPOSED_HEADERS);
-	}
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
 		// For PUT requests we need an extra round-trip
 		// See e.g. http://www.html5rocks.com/en/tutorials/cors/
 
-		String origin = request.getHeader(ORIGIN);
 		String acRequestMethod = request.getHeader(ACCESS_CONTROL_REQUEST_METHOD);
 		String acRequestHeaders = request.getHeader(ACCESS_CONTROL_REQUEST_HEADERS);
 
-		if (hasValue(origin) && hasValue(acRequestMethod)) {
+		// Our REST API is accessible from anywhere
+		response.setHeader(ACCESS_CONTROL_ALLOW_ORIGIN, "*");
+
+		if (HttpMethod.OPTIONS.toString().equals(request.getMethod()) && hasValue(acRequestMethod)) {
 			// this is a preflight check
 			// our API only needs this for PUT requests, anything we can PUT we can also GET
-			response.setHeader(ACCESS_CONTROL_ALLOW_ORIGIN,"*");
-			response.addHeader(ACCESS_CONTROL_ALLOW_METHODS, ALLOWED_METHODS);
-			response.setHeader(ACCESS_CONTROL_ALLOW_HEADERS,acRequestHeaders);
-			response.setHeader(ACCESS_CONTROL_MAX_AGE,CACHE_SECONDS);
-			response.addHeader(ACCESS_CONTROL_EXPOSE_HEADERS, EXPOSED_HEADERS);
-			
+			response.addHeader(ACCESS_CONTROL_ALLOW_METHODS, HttpMethod.GET.toString());
+			response.addHeader(ACCESS_CONTROL_ALLOW_METHODS, HttpMethod.POST.toString());
+			response.addHeader(ACCESS_CONTROL_ALLOW_METHODS, HttpMethod.PUT.toString());
+			response.setHeader(ACCESS_CONTROL_ALLOW_HEADERS, acRequestHeaders);
+			response.setHeader(ACCESS_CONTROL_MAX_AGE, CACHE_SECONDS);
+
 			return false; // Don't continue processing, return to browser immediately
+		}
+		else {
+			response.addHeader(ACCESS_CONTROL_EXPOSE_HEADERS, LOCATION);
 		}
 
 		return true; // Not a preflight check, continue as normal
