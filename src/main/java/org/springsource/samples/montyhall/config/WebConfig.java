e014860d43d8e90b2d299ee36fe92171980b0c87
Adrian Colyer <acolyer@vmware.com>
2012-10-10 10:01:35 +0100

    Remove internalViewResolver - not needed

--- src/main/java/org/springsource/samples/montyhall/config/WebConfig.java
+++ src/main/java/org/springsource/samples/montyhall/config/WebConfig.java
@@ -1,13 +1,10 @@
 package org.springsource.samples.montyhall.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springsource.samples.montyhall.web.CorsInterceptor;
 
 @Configuration
@@ -18,14 +15,6 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(new CorsInterceptor());
-	}
-
-	@Bean
-	public ViewResolver viewResolver() {
-		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-		viewResolver.setPrefix("/WEB-INF/jsp/");
-		viewResolver.setSuffix(".jsp");
-		return viewResolver;
 	}
 
 }
